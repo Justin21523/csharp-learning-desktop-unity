710621f5851bfbe87ce22ca66b4998ed1c0e3da4
Justin21523 <b0979865617@gmail>
2025-07-06 20:00:48 +0800

    chore(module1-unit1): relocate all unit1 samples and diagrams into unit1 folder

---
+++ module-01-csharp-basics/unit1/DateCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+// 取得今天日期
+DateTime today = DateTime.Now;
+Console.WriteLine($"Today: {today:yyyy-MM-dd}");
+
+// 計算自 2000-01-01 起的天數
+DateTime start = new DateTime(2000, 1, 1);
+double days = (today - start).TotalDays;
+Console.WriteLine($"Days since 2000-01-01: {days:N0}");
+
---
+++ module-01-csharp-basics/unit1/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+
+Console.WriteLine($"[Sync] ThreadId = {Environment.CurrentManagedThreadId}");
+Console.WriteLine("Hello, C#!");
+
+Console.WriteLine("Fetching https://dotnet.microsoft.com/ ...");
+int length = await GetContentLengthAsync("https://dotnet.microsoft.com");
+Console.WriteLine($"Bytes received = {length}");
+
+async Task<int> GetContentLengthAsync(string url)
+{
+    using HttpClient client = new();
+    Console.WriteLine($"[Async] ThreadId(before await) = {Environment.CurrentManagedThreadId}");
+    string html = await client.GetStringAsync(url);
+    Console.WriteLine($"[Async] ThreadId(after await) = {Environment.CurrentManagedThreadId}");
+    return html.Length;
+}
+
