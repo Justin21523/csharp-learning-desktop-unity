734da148b27bbb90d8fa0d39d1508c487816af6d
Justin21523 <b0979865617@gmail>
2025-07-06 22:07:22 +0800

    feat(module2-unit1): add Circle struct value-type exercise

---
+++ module-02-oop-delegates/unit1/CircleDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public struct Circle
+{
+    public double Radius { get; set; }
+    public double Area => Math.PI * Radius * Radius;
+    public Circle(double r) => Radius = r;
+}
+
+class Program
+{
+    static void Main()
+    {
+        var c1 = new Circle(5);
+        var c2 = c1;      // 值複製
+        c2.Radius = 10;
+        Console.WriteLine($"c1.Radius={c1.Radius}, Area={c1.Area}");
+        Console.WriteLine($"c2.Radius={c2.Radius}, Area={c2.Area}");
+    }
+}
